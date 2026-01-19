2f1ff3ca82ab7f904d5891252ea66e31626c4c61
Ricardo Godinez <rgodinezco@gmail.com>
2026-01-19 02:26:48 -0800

    fix: removed file

--- terraform/users.tf
+++
@@ -1,8 +0,0 @@
-# users.tf
-# resource "tailscale_user" "ricardo" {
-#   login_name = "github:r-godinez"
-# }
-
-# resource "tailscale_user" "sharlyne" {
-#   login_name = "sharlynemadrigal@gmail.com"
-# }
